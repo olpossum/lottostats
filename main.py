cf0e60272987635b7ef1f3ddc5eed563b036b2b7
Andy Welton <awelton1@gmail.com>
2022-08-06 15:45:02 -0500

    added in func for aggregating value counts

--- main.py
+++ main.py
@@ -47,25 +47,29 @@ def data_check(df):
         valid = 1
         return(valid, df)
 
-
-
-if __name__ == '__main__':
-    df = import_data(datapath)
-    cols = ['B1','B2','B3','B4','B5']
-    df[cols] = df['Winning Numbers'].str.split(' ', expand=True).astype(int)
-    #df[cols] = df[cols].apply(lambda x: x.str.lstrip('0'))
-
+def aggregate_frequency(df, cols):
     template = pd.Series(int(0),index=np.arange(1,71,1))
-
-    valid = 0
-    while valid == 0:
-        valid, df = data_check(df)
 
     vc1 = df['B1'].value_counts().sort_index().add(template,fill_value=0).astype(int)
     vc2 = df['B2'].value_counts().sort_index().add(template,fill_value=0).astype(int)
     vc3 = df['B3'].value_counts().sort_index().add(template,fill_value=0).astype(int)
     vc4 = df['B4'].value_counts().sort_index().add(template,fill_value=0).astype(int)
     vc5 = df['B5'].value_counts().sort_index().add(template,fill_value=0).astype(int)
+
+    agg_freq = vc1.add(vc2.add(vc3.add(vc4.add(vc5))))
+    print(agg_freq)
+if __name__ == '__main__':
+    df = import_data(datapath)
+    cols = ['B1','B2','B3','B4','B5']
+    df[cols] = df['Winning Numbers'].str.split(' ', expand=True).astype(int)
+    #df[cols] = df[cols].apply(lambda x: x.str.lstrip('0'))
+
+    valid = 0
+    while valid == 0:
+        valid, df = data_check(df)
+
+    aggregate_frequency(df,cols)
+
 
     #print(vc1)
     #print(vc2)
