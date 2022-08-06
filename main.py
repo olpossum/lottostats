5a4ebf28d802ace8d380f7b278f0d01910abc48c
Andy Welton <awelton1@gmail.com>
2022-08-06 13:28:40 -0500

    worked out value counts series that can now be added for aggregate frequency

--- main.py
+++ main.py
@@ -14,10 +14,16 @@ def import_data(datapath):
 if __name__ == '__main__':
     df = import_data(datapath)
     cols = ['B1','B2','B3','B4','B5']
-    df[cols] = df['Winning Numbers'].str.split(' ', expand=True)
+    df[cols] = df['Winning Numbers'].str.split(' ', expand=True).astype(int)
+    #df[cols] = df[cols].apply(lambda x: x.str.lstrip('0'))
 
-    vc1 = df['B1'].value_counts()
-    vc2 = df['B2'].value_counts()
-    vc3 = df['B3'].value_counts()
-    vc4 = df['B4'].value_counts()
-    vc5 = df['B5'].value_counts()
+    template = pd.Series(int(0),index=np.arange(1,71,1))
+
+    vc1 = df['B1'].value_counts().sort_index().add(template,fill_value=0).astype(int)
+    vc2 = df['B2'].value_counts().sort_index().add(template,fill_value=0).astype(int)
+    vc3 = df['B3'].value_counts().sort_index().add(template,fill_value=0).astype(int)
+    vc4 = df['B4'].value_counts().sort_index().add(template,fill_value=0).astype(int)
+    vc5 = df['B5'].value_counts().sort_index().add(template,fill_value=0).astype(int)
+
+    print(vc1)
+    print(vc2)
