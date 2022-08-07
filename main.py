eaba0d9abf4acacbeb81ee29982def38e2e341c8
Andy Welton <awelton1@gmail.com>
2022-08-06 22:14:50 -0500

    added ticket picker logic

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import csv
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import matplotlib as mp
 import os
+from collections import Counter
 
 datapath = os.path.normpath("/home/andy/PycharmProjects/lottostats/archive/Lottery_Mega_Millions_Winning_Numbers__Beginning_2002.csv")
 
@@ -59,7 +61,7 @@ def aggregate_frequency(df, cols):
     vc4 = df['B4'].value_counts().sort_index().add(template,fill_value=0).astype(int)
     vc5 = df['B5'].value_counts().sort_index().add(template,fill_value=0).astype(int)
 
-    vc6 = df['Mega Ball'].value_counts().sort_index()
+    vc6 = df['Mega Ball'].value_counts().sort_values(ascending=False)
 
     agg_freq = vc1.add(vc2.add(vc3.add(vc4.add(vc5))))
 
@@ -76,11 +78,66 @@ def top_frequency(freq):
     print(freq)
     return freq
 
-def pick_ticket(entries, fnum, fmega):
-    ticket = {}
+def pick_tickets(entries, fnum, fmega):
+    tickets = []
+    entry_i = 1
+    fnum_i = 0
+    print(fnum.index[fnum_i])
+    print(fnum.index[fnum_i+1])
+    print(fnum.index[fnum_i+2])
+    print(fnum.index[fnum_i+3])
+    print(fnum.index[fnum_i+4])
+    fmnum_i = 0
+    while entry_i <= entries:
+        try:
+            print("Loop " + str(entry_i))
+            entry_dict = {}
+            entry_dict.update({"B1": fnum.index[fnum_i]})
+            entry_dict.update({"B2": fnum.index[fnum_i+1]})
+            entry_dict.update({"B3": fnum.index[fnum_i+2]})
+            entry_dict.update({"B4": fnum.index[fnum_i+3]})
+            entry_dict.update({"B5": fnum.index[fnum_i+4]})
+            entry_dict.update({"M1": fmega.index[fmnum_i]})
+            tickets.append(entry_dict)
+            fnum_i += 5
+            fmnum_i += 1
+            entry_i += 1
+        except:
+            entry_dict = {}
+            flag = 0
+            unique = False
+            while unique == False:
+                random = np.random.randint(0,32,5)
+                print(random)
+                flag = len(set(random)) == len(random)
+                if(flag):
+                    unique = True
+                else:
+                    unique = False
+            random_mega = np.random.randint(0,25,1)
+            entry_dict.update({"B1": fnum.index[random[0]]})
+            entry_dict.update({"B2": fnum.index[random[1]]})
+            entry_dict.update({"B3": fnum.index[random[2]]})
+            entry_dict.update({"B4": fnum.index[random[3]]})
+            entry_dict.update({"B5": fnum.index[random[4]]})
+            entry_dict.update({"M1": fmega.index[random_mega[0]]})
+            tickets.append(entry_dict)
+            entry_i += 1
+
+    return tickets
+
+def dict_2_csv(dicts):
+    keys = dicts[0].keys()
+    print(keys)
+    with open('test.csv', 'w') as output_file:
+        dict_writer = csv.DictWriter(output_file,keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(dicts)
+
+
 
 if __name__ == '__main__':
-    entries = 5
+    entries = 10
     df = import_data(datapath)
     cols = ['B1','B2','B3','B4','B5']
     df[cols] = df['Winning Numbers'].str.split(' ', expand=True).astype(int)
@@ -96,4 +153,7 @@ if __name__ == '__main__':
     #plot_frequency(frequency_num)
     #plot_frequency(frequency_mega)
 
+    tickets = pick_tickets(entries, frequency_num, frequency_mega)
+    print(tickets)
 
+    dict_2_csv(tickets)
